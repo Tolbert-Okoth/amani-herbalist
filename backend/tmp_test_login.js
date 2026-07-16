055caa9946dedd6fa7bc126fa90a6d07b4b72b76
Tolbert Okoth <osumba30@gmail.com>
2026-07-16 22:36:11 +0300

    Security hygiene: remove compromised scripts, reorganize scripts, drop debug route, untrack binaries

--- backend/tmp_test_login.js
+++
@@ -1,9 +0,0 @@
-async function test() {
-  const req = await fetch('http://localhost:5001/api/auth/admin/login', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ username: 'stalin', password: '***REDACTED***' })
-  });
-  console.log(req.status, await req.json());
-}
-test();
